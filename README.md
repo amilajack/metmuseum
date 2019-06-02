6e70650545562198b16eb5fdf946d389dcd82744
Amila Welihinda <amilajack@gmail.com>
2019-06-02 14:36:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+metmuseum.js
+============
+
+A Node API for the [metmuseum API](https://metmuseum.github.io)
+
+## Installation
+
+```bash
+npm install metmuseum
+```
+
+## Local Setup
+
+```bash
+git clone https://github.com/amilajack/metmuseum
+cd metmuseum
+yarn
+yarn test
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+const ENDPOINTS = {
+    departments: 'https://collectionapi.metmuseum.org/public/collection/v1/departments',
+    search: 'https://collectionapi.metmuseum.org/public/collection/v1/search',
+    objects: 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
+};
+
+export default class API {
+    static getAllItems() {
+        return API.fetch(ENDPOINTS.objects);
+    }
+
+    static getItem(itemId) {
+        return API.fetch(ENDPOINTS.search + `/${itemId}`);
+    }
+
+    static fetch(url) {
+        return fetch(url).then(res => res.json());
+    }
+
+    static getDepartments() {
+        return API.fetch(ENDPOINTS.departments);
+    }
+
+    static search(query) {
+        return API.fetch(ENDPOINTS.search + `/q=${query}`);
+    }
+}
